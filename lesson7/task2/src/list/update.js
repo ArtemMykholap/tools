4b50ba57eb8e1bb7ee21b7bc77cb614eb5a89fe6
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-07-03 16:35:35 +0300

    todo plugin

---
+++ lesson7/task2/src/list/update.js
@@ -0,0 +1,25 @@
+// import { tasks } from './tasks.js';
+import { renderTasks, listElem } from './render.js';
+// import { createTask } from './createTask.js';
+import { updateServerTask, getTasksList } from './tasksGateway.js'
+
+
+
+function updateTask(event) {
+    getTasksList()
+        .then(tasks => {
+            const classes = event.target.classList;
+            if (!classes.contains('list__item-checkbox'))
+                return;
+            const tar = event.target
+            const task = tasks.find(task => task.id === event.target.dataset.taskId);
+            task.done = tar.checked
+            task.doneDate = new Date()
+                // setTasks(tasks)
+            updateServerTask(task)
+                .then(() => renderTasks(tasks))
+                // renderTasks(tasks)
+        })
+
+}
+export { updateTask };
